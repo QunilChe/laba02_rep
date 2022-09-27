e14b5d83e29b56b85bfa7ad556fa7a904f47ffd5
Qunilche <orest_teg@ukr.net>
2022-09-27 19:06:02 +0300

    02 commit

--- laba nomer dva/osnova.cpp
+++ laba nomer dva/osnova.cpp
@@ -8,12 +8,13 @@
 using namespace std;
 int main()
 {
+	double Pi = 4 * atan(1.); // число пі
 	double x; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
 	double z2; // результат обчислення 2-го виразу
 	cout << "x = "; cin >> x;
-	z1 = cos(x) + cos(x*2) + cos(x*6) + cos(x*7);
-	z2 = 4*cos(x/2)*cos((5/2)*x)*cos(4*x);
+	z1 = ((cos(x) + sin(x)) + (cos(3*x))) + (sin(3*x));
+	z2 = ((2 * sqrt(2)) * cos(x)) * (sin((Pi/4)+(2*x)));
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
